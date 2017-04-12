7efaceaea8e59a05855760a8ebcb7fc6ec31d153
Jaiden Mispy <jaiden@mispy.me>
2017-04-13 06:38:14 +0800

    Cleanup

--- src/UnityVector2.js
+++ src/UnityVector2.js
@@ -1,130 +1,130 @@
 export default class Vector2 {
-	static get kEpsilon() { return 1E-05 }
-	static get zero() { return new Vector2(0, 0) }
-	static get one() { return new Vector2(1, 1) }
-	static get up() { return new Vector2(0, 1) }
-	static get down() { return new Vector2(0, -1) }
-	static get left() { return new Vector2(-1, 0) }
-	static get right() { return new Vector2(1, 0) }
+  static get kEpsilon() { return 1E-05 }
+  static get zero() { return new Vector2(0, 0) }
+  static get one() { return new Vector2(1, 1) }
+  static get up() { return new Vector2(0, 1) }
+  static get down() { return new Vector2(0, -1) }
+  static get left() { return new Vector2(-1, 0) }
+  static get right() { return new Vector2(1, 0) }
 
-	static Lerp(a, b, t) {
-		t = Math.max(Math.min(t, 1), 0)
-		return new Vector2(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t)
-	}
+  static Lerp(a, b, t) {
+    t = Math.max(Math.min(t, 1), 0)
+    return new Vector2(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t)
+  }
 
-	static LerpUnclamped(a, b, t) {
-		return new Vector2(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t)
-	}
+  static LerpUnclamped(a, b, t) {
+    return new Vector2(a.x + (b.x - a.x) * t, a.y + (b.y - a.y) * t)
+  }
 
-	static MoveTowards(current, target, maxDistanceDelta) {
-		const a = target.Subtract(current)
-		const magnitude = a.magnitude
-		if (magnitude <= maxDistanceDelta || magnitude == 0)
-			return target
-		else
-			return current.Add(a).Divide(magnitude*maxDistanceDelta)
-	}
+  static MoveTowards(current, target, maxDistanceDelta) {
+    const a = target.Subtract(current)
+    const magnitude = a.magnitude
+    if (magnitude <= maxDistanceDelta || magnitude == 0)
+      return target
+    else
+      return current.Add(a).Divide(magnitude*maxDistanceDelta)
+  }
 
-	static Scale(a, b) {
-		return new Vector2(a.x*b.x, a.y*b.y)
-	}
+  static Scale(a, b) {
+    return new Vector2(a.x*b.x, a.y*b.y)
+  }
 
-	static Reflect(inDirection, inNormal) {
-		return inNormal.Multiply((-2 * Vector2.Dot(inNormal, inDirection))).Add(inDirection)
-	}
+  static Reflect(inDirection, inNormal) {
+    return inNormal.Multiply((-2 * Vector2.Dot(inNormal, inDirection))).Add(inDirection)
+  }
 
-	static Dot(lhs, rhs) {
-		return lhs.x * rhs.x + lhs.y * rhs.y
-	}
+  static Dot(lhs, rhs) {
+    return lhs.x * rhs.x + lhs.y * rhs.y
+  }
 
-	static Angle(from, to) {
-		return Math.acos(Math.max(Math.min(Vector2.Dot(from.normalized, to.normalized), 1), -1)) * 57.29578
-	}
+  static Angle(from, to) {
+    return Math.acos(Math.max(Math.min(Vector2.Dot(from.normalized, to.normalized), 1), -1)) * 57.29578
+  }
 
-	static Distance(a, b) {
-		return a.Subtract(b).magnitude
-	}
+  static Distance(a, b) {
+    return a.Subtract(b).magnitude
+  }
 
-	static ClampMagnitude(vector, maxLength) {
-		if (vector.sqrMagnitude > maxLength**2)
-			return vector.normalized.Multiply(maxLength)
-		else
-			return vector
-	}
+  static ClampMagnitude(vector, maxLength) {
+    if (vector.sqrMagnitude > maxLength**2)
+      return vector.normalized.Multiply(maxLength)
+    else
+      return vector
+  }
 
-	constructor(x, y) {
-		this.x = x
-		this.y = y
-	}
+  constructor(x, y) {
+    this.x = x
+    this.y = y
+  }
 
-	get 0() { return this.x }
-	set 0(v) { this.x = v }
-	get 1() { return this.y }
-	set 1(v) { this.y = v }
-	
-	Set(newX, newY) {
-		this.x = newX
-		this.y = newY
-	}
+  get 0() { return this.x }
+  set 0(v) { this.x = v }
+  get 1() { return this.y }
+  set 1(v) { this.y = v }
 
-	get normalized() {
-		const magnitude = this.magnitude
-		if (magnitude > 1E-05) {
-			return new Vector2(this.x/magnitude, this.y/magnitude)
-		} else {
-			return new Vector2(0, 0)
-		}
-	}
+  Set(newX, newY) {
+    this.x = newX
+    this.y = newY
+  }
 
-	get magnitude() {
-		return Math.sqrt(this.x**2 + this.y**2)
-	}
+  get normalized() {
+    const magnitude = this.magnitude
+    if (magnitude > 1E-05) {
+      return new Vector2(this.x/magnitude, this.y/magnitude)
+    } else {
+      return new Vector2(0, 0)
+    }
+  }
 
-	get sqrMagnitude() {
-		return this.x**2 + this.y**2
-	}
+  get magnitude() {
+    return Math.sqrt(this.x**2 + this.y**2)
+  }
 
-	Scale(scale) {
-		this.x *= scale.x
-		this.y *= scale.y
-	}
+  get sqrMagnitude() {
+    return this.x**2 + this.y**2
+  }
 
-	Normalize() {
-		const magnitude = this.magnitude
-		if (magnitude > 1E-05) {
-			this.x /= magnitude
-			this.y /= magnitude
-		} else {
-			this.x = 0
-			this.y = 0
-		}
-	}
+  Scale(scale) {
+    this.x *= scale.x
+    this.y *= scale.y
+  }
 
-	toString() {
-		return `(${this.x}, ${this.y})`
-	}
+  Normalize() {
+    const magnitude = this.magnitude
+    if (magnitude > 1E-05) {
+      this.x /= magnitude
+      this.y /= magnitude
+    } else {
+      this.x = 0
+      this.y = 0
+    }
+  }
 
-	Equals(other) {
-		if (!(other instanceof Vector2)) {
-			return false
-		} else {
-			return (other.Subtract(this)).sqrMagnitude < Vector2.kEpsilon
-		}
-	}
+  toString() {
+    return `(${this.x}, ${this.y})`
+  }
 
-	Add(other) {
-		return new Vector2(this.x+other.x, this.y+other.y)
-	}
+  Equals(other) {
+    if (!(other instanceof Vector2)) {
+      return false
+    } else {
+      return (other.Subtract(this)).sqrMagnitude < Vector2.kEpsilon
+    }
+  }
 
-	Subtract(other) {
-		return new Vector2(this.x-other.x, this.y-other.y)
-	}
+  Add(other) {
+    return new Vector2(this.x+other.x, this.y+other.y)
+  }
 
-	Multiply(number) {
-		return new Vector2(this.x*number, this.y*number)
-	}
+  Subtract(other) {
+    return new Vector2(this.x-other.x, this.y-other.y)
+  }
 
-	Divide(number) {
-		return new Vector2(this.x/number, this.y/number)
-	}	
+  Multiply(number) {
+    return new Vector2(this.x*number, this.y*number)
+  }
+
+  Divide(number) {
+    return new Vector2(this.x/number, this.y/number)
+  }
 }
--- test/UnityVector2.js
+++ test/UnityVector2.js
@@ -2,108 +2,108 @@ import Vector2 from '../src/UnityVector2'
 import {expect} from 'chai'
 
 describe('Vector2', () => {
-	it('static get kEpsilon', () => {
-		expect(Vector2.kEpsilon).to.be.below(0.0001)
-	})
+  it('static get kEpsilon', () => {
+    expect(Vector2.kEpsilon).to.be.below(0.0001)
+  })
 
-	it('static Lerp(a, b, t)', () => {
-		const v = Vector2.Lerp(new Vector2(1, 1), new Vector2(3, 3), 0.5)
-		expect(v.Equals(new Vector2(2, 2))).to.be.true
-	})
+  it('static Lerp(a, b, t)', () => {
+    const v = Vector2.Lerp(new Vector2(1, 1), new Vector2(3, 3), 0.5)
+    expect(v.Equals(new Vector2(2, 2))).to.be.true
+  })
 
-	it('static LerpUnclamped(a, b, t)', () => {
-		const v = Vector2.LerpUnclamped(new Vector2(1, 1), new Vector2(3, 3), 2)
-		expect(v.Equals(new Vector2(5, 5))).to.be.true
-	})
+  it('static LerpUnclamped(a, b, t)', () => {
+    const v = Vector2.LerpUnclamped(new Vector2(1, 1), new Vector2(3, 3), 2)
+    expect(v.Equals(new Vector2(5, 5))).to.be.true
+  })
 
-	it('static MoveTowards(current, target, maxDistanceDelta)', () => {
-		const v = Vector2.MoveTowards(new Vector2(1, 1), new Vector2(5, 5), 100)
-		expect(v.Equals(new Vector2(5, 5))).to.be.true
-	})
+  it('static MoveTowards(current, target, maxDistanceDelta)', () => {
+    const v = Vector2.MoveTowards(new Vector2(1, 1), new Vector2(5, 5), 100)
+    expect(v.Equals(new Vector2(5, 5))).to.be.true
+  })
 
-	it('static Scale(a, b)', () => {
-		const v = Vector2.Scale(new Vector2(2, 2), new Vector2(2, 1))
-		expect(v.Equals(new Vector2(4, 2))).to.be.true
-	})
+  it('static Scale(a, b)', () => {
+    const v = Vector2.Scale(new Vector2(2, 2), new Vector2(2, 1))
+    expect(v.Equals(new Vector2(4, 2))).to.be.true
+  })
 
-	it('static Reflect(inDirection, inNormal)', () => {
-		const v = Vector2.Reflect(new Vector2(1, 2), new Vector2(2, 4))
-		expect(v.Equals(new Vector2(-39, -78))).to.be.true
-	})
+  it('static Reflect(inDirection, inNormal)', () => {
+    const v = Vector2.Reflect(new Vector2(1, 2), new Vector2(2, 4))
+    expect(v.Equals(new Vector2(-39, -78))).to.be.true
+  })
 
-	it('static Dot(lhs, rhs)', () => {
-		const result = Vector2.Dot(new Vector2(1, 2), new Vector2(2, 3))
-		expect(result).to.eq(8)
-	})
+  it('static Dot(lhs, rhs)', () => {
+    const result = Vector2.Dot(new Vector2(1, 2), new Vector2(2, 3))
+    expect(result).to.eq(8)
+  })
 
-	it('static Angle(from, to)', () => {
-		const result = Vector2.Angle(new Vector2(0, 1), new Vector2(1, 0))
-		expect(result-90).to.be.below(Vector2.kEpsilon)
-	})
+  it('static Angle(from, to)', () => {
+    const result = Vector2.Angle(new Vector2(0, 1), new Vector2(1, 0))
+    expect(result-90).to.be.below(Vector2.kEpsilon)
+  })
 
-	it('static Distance(from, to)', () => {
-		const result = Vector2.Distance(new Vector2(0, 0), new Vector2(0, 1))
-		expect(result).to.eq(1)
-	})
+  it('static Distance(from, to)', () => {
+    const result = Vector2.Distance(new Vector2(0, 0), new Vector2(0, 1))
+    expect(result).to.eq(1)
+  })
 
-	it('static ClampMagnitude(vector, maxLength)', () => {
-		const result = Vector2.ClampMagnitude(new Vector2(10, 10), 1)
-		expect(result.magnitude-1).to.be.below(Vector2.kEpsilon)
-	})
+  it('static ClampMagnitude(vector, maxLength)', () => {
+    const result = Vector2.ClampMagnitude(new Vector2(10, 10), 1)
+    expect(result.magnitude-1).to.be.below(Vector2.kEpsilon)
+  })
 
-	it('has 0 and 1 properties', () => {
-		const v = new Vector2(1, 1)
-		expect(v[0]).to.eq(1)
-		expect(v[1]).to.eq(1)
-		v[0] = 2
-		v[1] = 3
-		expect(v[0]).to.eq(2)
-		expect(v[1]).to.eq(3)
-	})
+  it('has 0 and 1 properties', () => {
+    const v = new Vector2(1, 1)
+    expect(v[0]).to.eq(1)
+    expect(v[1]).to.eq(1)
+    v[0] = 2
+    v[1] = 3
+    expect(v[0]).to.eq(2)
+    expect(v[1]).to.eq(3)
+  })
 
-	it('get normalized', () => {
-		expect(new Vector2(1, 1).normalized.x).to.eq(0.7071067811865475)
-		expect(new Vector2(1, 1).normalized.y).to.eq(0.7071067811865475)
-	})
+  it('get normalized', () => {
+    expect(new Vector2(1, 1).normalized.x).to.eq(0.7071067811865475)
+    expect(new Vector2(1, 1).normalized.y).to.eq(0.7071067811865475)
+  })
 
-	it('get magnitude', () => {
-		expect(new Vector2(1, 1).magnitude).to.eq(1.4142135623730951)
-	})
+  it('get magnitude', () => {
+    expect(new Vector2(1, 1).magnitude).to.eq(1.4142135623730951)
+  })
 
-	it('get sqrMagnitude', () => {
-		expect(Math.sqrt(new Vector2(1, 1).sqrMagnitude)).to.eq(new Vector2(1, 1).magnitude)
-	})
+  it('get sqrMagnitude', () => {
+    expect(Math.sqrt(new Vector2(1, 1).sqrMagnitude)).to.eq(new Vector2(1, 1).magnitude)
+  })
 
-	it('Equals(other)', () => {
-		const a = new Vector2(1, 1)
-		const b = new Vector2(1, 1)
-		const c = new Vector2(2, 2)
+  it('Equals(other)', () => {
+    const a = new Vector2(1, 1)
+    const b = new Vector2(1, 1)
+    const c = new Vector2(2, 2)
 
-		expect(a.Equals(b)).to.be.true
-		expect(a.Equals(c)).to.be.false
-	})
+    expect(a.Equals(b)).to.be.true
+    expect(a.Equals(c)).to.be.false
+  })
 
-	it('Add(other)', () => {
-		const a = new Vector2(0, 1)
-		const b = new Vector2(1, 2)
+  it('Add(other)', () => {
+    const a = new Vector2(0, 1)
+    const b = new Vector2(1, 2)
 
-		expect(a.Add(b).Equals(new Vector2(1, 3))).to.be.true
-	})
+    expect(a.Add(b).Equals(new Vector2(1, 3))).to.be.true
+  })
 
-	it('Subtract(other)', () => {
-		const a = new Vector2(1, 3)
-		const b = new Vector2(0, 1)
+  it('Subtract(other)', () => {
+    const a = new Vector2(1, 3)
+    const b = new Vector2(0, 1)
 
-		expect(a.Subtract(b).Equals(new Vector2(1, 2))).to.be.true
-	})
+    expect(a.Subtract(b).Equals(new Vector2(1, 2))).to.be.true
+  })
 
-	it('Multiply(number)', () => {
-		const a = new Vector2(1, 2)
-		expect(a.Multiply(10).Equals(new Vector2(10, 20))).to.be.true
-	})
+  it('Multiply(number)', () => {
+    const a = new Vector2(1, 2)
+    expect(a.Multiply(10).Equals(new Vector2(10, 20))).to.be.true
+  })
 
-	it('Divide(number)', () => {
-		const a = new Vector2(10, 20)
-		expect(a.Divide(10).Equals(new Vector2(1, 2))).to.be.true
-	})
+  it('Divide(number)', () => {
+    const a = new Vector2(10, 20)
+    expect(a.Divide(10).Equals(new Vector2(1, 2))).to.be.true
+  })
 });
